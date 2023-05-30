bc497d04238847840ddc6322bb277580d7e15ed1
MAjay2 <adjeimichael405@gmail.com>
2023-05-30 08:40:43 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Moviereviews from "./components/Moviereviews"
+
+function App() {
+  
+  return (
+    <div>
+      <Moviereviews/>
+    </div>
+  )
+}
+
+export default App
